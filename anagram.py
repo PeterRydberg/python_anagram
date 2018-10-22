57fbc426fb7233cbeb912a8ec44ddad7a235f0d2
Peter Rydberg <peter.rydberg@hotmail.no>
2018-10-22 08:46:44 +0200

    Added naive approach

--- anagram.py
+++ anagram.py
@@ -1,6 +1,23 @@
+import codecs
+
 filename = "eventyr.txt"
-file = open(filename, "r")
+file = codecs.open(filename, encoding='utf-8')
 
-words = [line.strip('\n') for line in file]
+words = [line.strip('\r\n') for line in file]
 
-print(words)
+# Compares word for anagram check
+def compare(word1, word2):
+    sort1 = sorted(word1)
+    sort2 = sorted(word2)
+
+    if(sort1 == sort2):
+        print(word1, "and", word2, "are anagrams")
+
+# Bad runtime with duplicates
+def anagram_naive():
+    for word1 in words:
+        for word2 in words:
+            compare(word1, word2)
+
+anagram_naive()
+
